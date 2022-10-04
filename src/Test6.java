c167630a6034f42f978b8bdd11c6b03b78179a90
WeiFengWa <2969079275@qq.com>
2022-10-04 09:48:22 +0800

    提交

--- src/Test6.java
+++ src/Test6.java
@@ -1,7 +1,7 @@
 public class Test6 {
     public static void main(String[] args) {
         Animal cat = new Animal();
-        cat.setName("喵桑");
+        cat.setName("喵桑啊");
         cat.setSex('男');
         cat.setColor("白色");
         System.out.println(cat.getName());
