febdd6e056a4b756af5d8ff01826554dbdc3860b
WeiFengWa <2969079275@qq.com>
2022-10-11 09:08:42 +0800

    提交

---
+++ src/Test10.java
@@ -0,0 +1,32 @@
+public class Test10 {
+
+    public static void main(String[] args) {
+
+        Week week = Week.Saturday;
+        String msg = "今天";
+        switch (week) {
+            case Monday:
+                msg += "星期一，心情郁闷";
+                break;
+            case Tuesday:
+                msg += "星期二，心情不错";
+                break;
+            case Wednesday:
+                msg += "星期三，心情好";
+                break;
+            case Thursday:
+                msg += "星期四，心情非常好";
+                break;
+            case Friday:
+                msg += "星期五，高兴得飞起来";
+                break;
+            case Saturday:
+                msg += "星期六，心情异常兴奋";
+                break;
+            case Sunday:
+                msg += "星期日，心情又开始郁闷";
+                break;
+        }
+        System.out.println(msg);
+    }
+}
